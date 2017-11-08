0611ea7f3fe64355a48d26702f00315e943265d7
Suman Paul <sumank.paul@gmail.com>
2017-11-08 16:18:49 +0700

    added react

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-const name = 'Suman';
-console.log(`Hello world from ${name}!`);
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
