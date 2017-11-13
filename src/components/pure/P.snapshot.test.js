3fd9f72796347aaf51ed86777f9c86148e434e51
Suman Paul <sumank.paul@gmail.com>
2017-11-13 06:39:44 +0530

    adding file type association to snapshot files, and flow-to-proptype for runtime validation

--- src/components/pure/P.snapshot.test.js
+++ src/components/pure/P.snapshot.test.js
@@ -4,14 +4,14 @@ import renderer from 'react-test-renderer';
 
 it('renders correctly content 1', () => {
   const tree = renderer.create(
-    <P content="content 1"/>
+    <P content="content 1" />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it('renders correctly content 2', () => {
   const tree = renderer.create(
-    <P content="content 2"/>
+    <P content="content 2" />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
