8270364cd711aba67d6e93a51246f54fe7984ca6
Suman Paul <sumank.paul@gmail.com>
2018-01-07 07:27:23 +0700

    added router

--- src/app.js
+++ src/app.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-import { P } from 'Components'
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Routes = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+    </div>
+  </Router>
+)
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        Hello from react
-        <P content='hi' />
-        <P />
+        <Routes />
       </div>
     )
   }
